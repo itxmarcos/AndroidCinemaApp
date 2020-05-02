e4d3c036d16d4aa791538b96ed47e6dbb0130963
Marcos Caballero <m.caballeroyus@gmail.com>
2020-05-02 09:40:50 +0200

    Final commit in test branch

--- app/src/main/java/com/example/cinemaapp/MainActivity.kt
+++ app/src/main/java/com/example/cinemaapp/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_list)
+        setContentView(R.layout.activity_main)
     }
 }
